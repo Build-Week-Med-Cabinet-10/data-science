497e54a0906b14176b2c0be2559c8c8e6b1be616
joshua piche <jpiche0720@gmail.com>
2020-06-20 09:18:17 -0400

    return JSON, sample demo

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,15 +1,29 @@
 from flask import Flask
-from web_app.routes.home_routes import home_routes
+#from sqlalchemy import create_engine
+#from web_app.db_models import db, migrate, DATABASE_URI, DF_FILEPATH
 from web_app.routes.stats_routes import stats_routes
+import pandas
+
+
 
 
 def create_app():
     app = Flask(__name__)
     
-    app.register_blueprint(home_routes)
+
+
+    ######## Configure Database for Strain and User Recommendation Storage 
+    # app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    # db.init_app(app)
+    # migrate.init_app(app, db)
+    
+    # engine = create_engine(DATABASE_URI, echo=False)
+    # df = pandas.read_csv(DF_FILEPATH)
+    # df.to_sql(name = 'Strains',con=engine, if_exists='replace',index_label= 'id')
+
+    
     app.register_blueprint(stats_routes)
     
-
     return app
 
 if __name__ == "__main__":
--- web_app/routes/stats_routes.py
+++ web_app/routes/stats_routes.py
@@ -5,13 +5,20 @@ import pickle
 import sklearn
 import pandas
 
-
 stats_routes = Blueprint("stats_routes", __name__)
 
+@stats_routes.route('/recommendation/new')
+
+def sample_form():
+    print('Visiting Recommendation Page')
+    return render_template('sample_form.html', methods=['GET'])
 
 @stats_routes.route("/recommendation/new", methods=["POST"])
 
 def predict():
+    '''
+    Returns a JSON object of the recommended strain information. 
+    '''
     # Load Model
     KNN_FILEPATH = 'web_app/data/knn.pkl'
     print("LOADING THE MODEL...")
@@ -30,23 +37,25 @@ def predict():
     with open(FLAVOR_FILEPATH, "rb") as flavors_file:
         flavors = pickle.load(flavors_file)
     
+    # Generate Recommendation
     print("PREDICT ROUTE...")
     print("FORM DATA:", dict(request.form))
+
     from_web = dict(request.form)
     eff = request.form['effect']
     fla = request.form['flavor']
-    print(eff)
-    print(fla)
+    print('USING', eff)
+    print('USING', fla)
+    
     #1. Web team sends this user query in some format(probably json)
-    #from_web = {'effect':'Aroused', 'flavor': 'Sweet'} #json like object 
+    # example = from_web = {'effect':'Aroused', 'flavor': 'Sweet'} -> json like object 
     
     #2. Use info to get vectors from pickled dictionaries
-    #
     effect = effects[list(from_web.values())[0]]
     flavor = flavors[list(from_web.values())[1]]
+
     #3. Generate query vector by adding these vectors
     query = effect + flavor
-
 
     #4. Run knn model using query vector. Needs to be reshaped
     result = saved_model.kneighbors(query.reshape(1,-1))
@@ -54,17 +63,19 @@ def predict():
     df = pandas.read_csv(DF_FILEPATH)
 
     #5. Result object will have the index location of recomendations to lookup in df
+    strains = df.iloc[result[1][0]]['Strain'].to_list() 
+    row = df[df['Strain']== strains[0]]
+    dictionary = row.to_dict()
+    
+   
+    
+    return jsonify(dictionary) 
+    #return render_template("results.html",
+        # effect= eff,
+        # flavor= fla,
+        # strains= strains[0])
 
-    strains = df.iloc[result[1][0]]['Strain'].tolist()
 
 
-    #print(jsonify(result))   
-
-    return render_template("results.html",
-        effect= eff,
-        flavor= fla,
-        
-        strains= strains[0])
-
-
-
+if __name__ == '__main__':
+    predict()
